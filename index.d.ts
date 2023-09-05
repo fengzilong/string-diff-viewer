8674ba89fada305ca20875aad67c39092b12f306
MO <fengzilong1992@gmail.com>
2023-09-06 02:32:55 +0800

    chore: bump version

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 interface FormatOptions {
   disableSyntaxHighlight?: boolean
   forceSideBySide?: boolean
+  columns?: number | ((termWidth: number) => number)
 }
 export function diff(oldStr: string, newStr: string): Promise<string>
 export function format(diffs?: string, options?: FormatOptions): Promise<string>
