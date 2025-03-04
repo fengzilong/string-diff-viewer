e9bb9835133f65f722a7458005583222cdc55eb3
MO <fengzilong1992@gmail.com>
2025-03-04 22:57:32 +0800

    fix: fix types in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,14 @@
-interface FormatOptions {
-  disableSyntaxHighlight?: boolean
-  forceSideBySide?: boolean
-  columns?: number | ((termWidth: number) => number)
+interface DiffOptions {
+  language?: string;
+  theme?: string;
+  syntaxTheme?: string;
+  displayFilename?: string;
+  sideBySide?: boolean;
+  columns?: number;
 }
-export function diff(oldStr: string, newStr: string): Promise<string>
-export function format(diffs?: string, options?: FormatOptions): Promise<string>
+
+export function diff(
+  oldSource: string,
+  newSource: string,
+  options?: DiffOptions
+): Promise<string>
